9494af67046f9180cebd257799c84a565e6a0d69
Huzaifa <hoozayfa@gmail.comm>
2024-06-01 23:14:09 +0500

    Reviews4

--- src/ResultPage.js
+++ src/ResultPage.js
@@ -63,7 +63,7 @@ const ResultPage = () => {
         <Message>
           <p>{userName}, if you spent as much time studying as you just did here, you'd already have one topic mastered.</p>
           <p>Your commitment to procrastination is truly impressive. Maybe start studying next year?</p>
-          <p>Seriously, {userName}? You think you’re going to magically understand genomic and industrial processes, you genius slacker? </p> 
+          <p>Seriously, {userName}? You think you’re going to magically understand whole of notes in one night, you genius slacker? </p> 
           <p>Absolutely not.</p>
           <p>You're not like your teachers – "na demagh na tension."</p>
         </Message>
