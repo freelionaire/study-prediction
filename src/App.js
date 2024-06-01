d48f0281122d81d760199e5e10dcef077f4d562f
Huzaifa <hoozayfa@gmail.comm>
2024-06-01 20:27:23 +0500

    Add @vercel/analytics component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './MainPage';
 import Questionnaire from './Questionnaire';
 import ResultPage from './ResultPage';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/questionnaire" element={<Questionnaire />} />
         <Route path="/result" element={<ResultPage />} />
       </Routes>
+      <Analytics />
     </Router>
   );
 }
