99462020f1e0ceac0f1532f5c4b4d463bd864845
Huzaifa <hoozayfa@gmail.comm>
2024-06-01 20:59:16 +0500

    Reviews4

--- src/MainPage.js
+++ src/MainPage.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 
 const MainPageContainer = styled.div`
+margin-top: 20rev;
   display: flex;
   flex-direction: column;
   align-items: center;
